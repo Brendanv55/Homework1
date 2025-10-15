9c9e19a45dacd95493c5f083e7786be0ddad25e7
Brendan V <brendanvalleau@gmail.com>
2025-10-16 00:50:16 +0530

    Initial Commit.

---
+++ src/producer.cpp
@@ -0,0 +1,64 @@
+
+#include <iostream>
+#include "pthread.h"
+#include <semaphore.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <cstring>
+// #include <string.h>
+
+typedef struct {
+        int in = 0;
+        int out = 0;
+        int buffer[2] = {};
+} data;
+
+//Semaphore
+const char* name = "brv_smph";
+sem_t* smphre = sem_open(name, 1);
+
+//Shared memory init
+data* data_ptr = new data;
+
+key_t key = 200;
+size_t size = sizeof(&data_ptr);
+int shmflag = 1;
+int shmID = shmget(key, size, shmflag);
+
+void* smaddr = 0;
+void* addr = shmat(shmID, smaddr, shmflag);
+// Copy my struct into the shared memory segment
+void * _mcpyaddr = memcpy(addr, data_ptr, sizeof(data));
+int detach = shmdt(addr);
+
+//produce method
+void *produce(void *ptr);
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        // pthread_t threadId;
+        // int success = pthread_create(&threadId, NULL, produce, NULL); // Create thread
+        // pthread_join(threadId, NULL); // Blocking wait
+    }
+}
+
+void *produce(void *ptr) {
+    sem_wait(smphre); // wait to enter critical section
+
+    void* addr = shmat(shmID, smaddr, shmflag); // Acquire shared memory
+
+    data* shared_data = (data *) smaddr;
+
+    while ((shared_data->out + 1) % 2 == shared_data->in) {};
+
+
+    shared_data->buffer[shared_data->in] = 1; // Message 
+    shared_data->in = (shared_data->in + 1) % 2; // Circular queuei++
+    
+    detach = shmdt(addr);
+
+    sem_post(smphre); // release
+    pthread_exit(0);
+}
